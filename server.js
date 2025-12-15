db1818d2cd6b38e62b4ddc129f5f68c273a2c475
mcavusoglu103-eng <mcavusoglu103@gmail.com>
2025-12-15 12:47:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); // body-parser yerine bu yeterli
 
 app.get("/", (req, res) => {
   res.send("SohbetX API çalışıyor");
 });
 
 app.post("/chat", (req, res) => {
-  const msg = req.body.message || "";
-  res.json({ reply: "Mesaj alındı: " + msg });
+  const msg = req.body.message;
+
+  if (!msg) {
+    return res.json({ reply: "Mesaj gelmedi" });
+  }
+
+  res.json({
+    reply: "Mesaj alındı: " + msg
+  });
 });
 
 const PORT = process.env.PORT || 3000;
