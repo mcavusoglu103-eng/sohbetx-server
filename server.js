9ac7fc56a6735723ed74d2a524f5acd329cc5fd7
mcavusoglu103-eng <mcavusoglu103@gmail.com>
2025-12-18 10:05:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+app.post("/chat", (req, res) => {
+  const message = req.body?.message ?? "";
+  res.json({ reply: "Mesaj alındı: " + message });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
