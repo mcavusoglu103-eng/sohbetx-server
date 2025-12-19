2331315efac6f2f06c2c4bd5085fe7d55507c39f
mcavusoglu103-eng <mcavusoglu103@gmail.com>
2025-12-19 23:04:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const app = express();
 app.use(express.json({ limit: "1mb" }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 
 const GEMINI_KEY = process.env.SohbetX || process.env.GEMINI_API_KEY;
 
