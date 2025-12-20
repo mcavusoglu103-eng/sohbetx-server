d9750811a65b3a708d45e4eaf8554ea874bc3548
mcavusoglu103-eng <mcavusoglu103@gmail.com>
2025-12-20 12:38:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -65,7 +65,8 @@ app.post("/chat", async (req, res) => {
     const prompt = `${histText}\nSen: ${message}\nBot:`;
 
     // Ücretsiz / hızlı model
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+;
 
     const result = await model.generateContent(prompt);
     const text = result?.response?.text?.() || "Cevap üretilemedi.";
